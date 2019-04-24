aa4f01ad799cf17a414999ec320c3ec67ddb0e0c
jamespagedev <jamespagedev@outlook.com>
2019-04-24 11:03:45 -0700

    ex2

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -40,3 +40,21 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+
+// ****************************************** fork template ******************************************
+/*
+int rc = fork();
+
+if (rc < 0) // fork failed; exit
+{
+  fprintf(stderr, "fork failed\n");
+  exit(1);
+}
+else if (rc == 0) // child process satisfies this branch
+{
+  exit(0);
+}
+waitpid(rc, NULL, 0);
+or...
+wait(NULL);
+*/
